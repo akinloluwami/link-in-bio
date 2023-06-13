bde48eca2211a24ade7b0db48919898d86cda265
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-13 02:26:51 +0100

    removed aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 const colors = {
   brand: {
-    100: "#ee0f3e",
+    100: "#fe90e7",
   },
 };
 
 export const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    AOS.init();
-  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
