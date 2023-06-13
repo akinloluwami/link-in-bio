0205ec04949b3412108eacdad1343af7d4dbf791
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-13 02:03:11 +0100

    initial commit

---
+++ interfaces.ts
@@ -0,0 +1,15 @@
+export interface LinkProps {
+  url: string;
+  title: string;
+}
+
+export interface ProfileProps {
+  name: string;
+  bio: string;
+  image: string;
+}
+
+export interface SocialLinkProps {
+  url: string;
+  platform: string;
+}
