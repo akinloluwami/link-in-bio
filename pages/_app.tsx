44cea0e6cdc191643d2dac7ec1cd3784e9e9acac
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-13 02:19:47 +0100

    aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const colors = {
   brand: {
-    100: "#fee332",
+    100: "#ee0f3e",
   },
 };
 
 export const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
