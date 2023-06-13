b5a2e105790204feffa090951dcd69b270b6f5e0
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-13 02:03:34 +0100

    initial commit

---
+++ components/Links.tsx
@@ -0,0 +1,33 @@
+import { Flex, Link } from "@chakra-ui/react";
+import { LinkProps } from "@/interfaces";
+
+const Links = ({ links }: { links: LinkProps[] }) => {
+  return (
+    <Flex flexDirection={"column"} alignItems={"center"} gap={6}>
+      {links.map((link, i) => (
+        <Link
+          key={i}
+          href={link?.url}
+          bgColor={"white"}
+          w={"90%"}
+          maxW={"400px"}
+          textAlign={"center"}
+          fontWeight={"semibold"}
+          py={3}
+          boxShadow={"5px 7px"}
+          fontSize={"20px"}
+          _hover={{
+            boxShadow: "10px 15px",
+            transform: "scale(1.02)",
+          }}
+          isExternal
+          border={"2px solid"}
+        >
+          {link?.title}
+        </Link>
+      ))}
+    </Flex>
+  );
+};
+
+export default Links;
